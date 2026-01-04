05b4101d25237118b59b179edfa55a3dde54e044
SeedCats <s928641007@gmail.com>
2026-01-04 20:35:54 +0800

    add origins for render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Allowed origins
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:4173'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:4173', 'https://my-pwa-app-front.onrender.app'];
 
 // Only allows this origin to access the server
 app.use(cors({
